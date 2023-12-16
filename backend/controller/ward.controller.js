2661a3e89cfe6608eb39df3ddcfc294bc2cb32fe
praveen kumar <praveenshakya1@gmail.com>
2023-12-16 22:15:53 +0530

    ward api's completed

---
+++ backend/controller/ward.controller.js
@@ -0,0 +1,98 @@
+import Ward from "../models/ward.model.js"
+
+
+const getAllWard = async(req, res)=>{
+    try {
+        const wards = await Ward.find({});
+        res.status(200).json({
+            success: true,
+            wards,
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Error while retrieving all Ward"
+        })
+    }
+};
+
+
+// add Ward
+
+const addWard = async(req, res)=>{
+    console.log("Adding Ward", req.body);
+    
+    try {
+        const {wardNumber, capacity} = req.body;
+        if(!wardNumber || !capacity){
+            return res.status(404).json({
+                success: false,
+                message:"Enter a Ward number or capacity"
+            })
+        }
+
+        const ward = await Ward.create(req.body);
+        res.status(201).json({
+            success: true,
+            message:"Added Ward Successfully",
+            ward
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Error while adding Ward"
+        })
+    }
+};
+
+const updateWard = async(req, res)=>{
+    try {
+        const ward = await Ward.findById(req.params.id);
+        if(!ward){
+            return res.status(404).json({
+                success: false,
+                message:"ward not found",
+            })
+        }
+
+        await ward.updateOne(req.body);
+        res.status(200).json({
+            success: true,
+            message:"Updated Ward successfully",
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Error while updating the Ward"
+        })
+    }
+};
+
+
+const deleteWard = async(req, res)=>{
+    try {
+        const ward = await Ward.findById(req.params.id);
+        if(!ward){
+            return res.status(404).json({
+                success: false,
+                message:"ward not found",
+            })
+        }
+
+        await ward.deleteOne();
+        res.status(200).json({
+            success: true,
+            message:"deleted Ward successfully",
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Error while deleting the Ward"
+        })
+    }
+}
+
+
+
+export {getAllWard, addWard, deleteWard, updateWard};
--- backend/router/ward.route.js
+++ backend/router/ward.route.js
@@ -1,8 +1,12 @@
 import express from 'express';
-
-
+import { addWard, getAllWard, updateWard, deleteWard } from '../controller/ward.controller.js';
 
 const wardRouter = express.Router();
 
+
+wardRouter.get('/', getAllWard);
+wardRouter.post('/new', addWard);
+wardRouter.put('/update/:id', updateWard);
+wardRouter.delete('/delete/:id', deleteWard);
 
 export default wardRouter;
